b1d06066c1457863f85d6e24123163feddd5b2f6
Allan Duarte <allanantonucci47@gmail.com>
2022-08-23 08:45:19 -0300

    Criado logica de caixa

---
+++ Primeiros/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Primeiros
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string nome2;
+        public int idade;
+        public int idade2;
+        public double salario;
+        public double salario2;
+
+        public string Result()
+        {
+            if(idade > idade2)
+            {
+                return this.nome + " Tem a idade Maior";
+            }
+            else if(idade2 > idade)
+            {
+                return this.nome2 + " Tem a idade Maior";
+            }
+            else
+            {
+                return "Idades iguais";
+            }
+        }
+
+        public double SalarioMedia()
+        {
+           return (this.salario + this.salario2) / 2;
+        }
+    }
+}
---
+++ Primeiros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Dados Primeira Pessoa");
+
+            Console.Write("Nome: ");
+            pessoa.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa.idade = int.Parse(Console.ReadLine());
+            Console.Write("Salario: ");
+            pessoa.salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nDados Segunda Pessoa");
+
+            Console.Write("Nome: ");
+            pessoa.nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa.idade2 = int.Parse(Console.ReadLine());
+            Console.Write("Salario: ");
+            pessoa.salario2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\n"+pessoa.Result());
+            Console.WriteLine("\nMedia dos salários: "+ pessoa.SalarioMedia().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
