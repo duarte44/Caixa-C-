4febd577baf736dc56edb0180c1741a95aa18859
Allan Duarte <85140775+duarte44@users.noreply.github.com>
2022-08-24 10:15:32 -0300

    Add files via upload

---
+++ Registro/Dados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro
+{
+    internal class Dados
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double salary { get; private set; }
+
+        public Dados(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            this.salary = this.salary + (this.salary * (percentage / 100));
+        }
+
+        public override string ToString()
+        {
+            return this.id + ", " + this.name + ", "+ this.salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Registro/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered ? ");
+            int nm = int.Parse(Console.ReadLine());
+
+
+            List<Dados> list = new List<Dados>();
+
+
+            for (int i = 0; i < nm; i++)
+            {
+                Console.WriteLine("Emplyoee #" + (i + 1));
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+                 
+                Dados dados = new Dados(id, name, salary);
+                list.Add(dados);
+
+                Console.WriteLine();
+               
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int inc = int.Parse(Console.ReadLine());
+
+
+
+            Dados emp = list.Find(x => x.id == inc);
+
+            if (emp != null)
+                {
+                    Console.Write("Enter the percentage: ");
+                    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    emp.increaseSalary(percentage);
+                }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Updated list of employees:");
+            
+            foreach(Dados dados in list)
+            {
+                Console.WriteLine(dados);
+            }
+            
+            
+
+        }
+    }
+}
